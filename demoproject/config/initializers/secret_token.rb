78e99e2c1f6158597d284da856562bf6935538c9
Lisa Friedman <lisafriedman@new-host-3.home>
2013-05-11 09:30:39 -0700

    first commit

---
+++ demoproject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoproject::Application.config.secret_token = 'fea97989f59d071f6a1c07d2d14b17d547040c1141882144ef462849f3c8ef0b7d919a575da1d4f7600c101d78d7a9e42466e06a0a2be76853044e3759490a5a'
